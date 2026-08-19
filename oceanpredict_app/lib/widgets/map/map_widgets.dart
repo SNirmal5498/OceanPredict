33d54f3a2896d93a2e6ed45da4d08d0dcdba7aa4
Nirmal-S <nirmalsundaram5498@gmail.com>
2026-08-19 21:56:55 +0530

    analytics page error corrected

--- oceanpredict_app/lib/widgets/map/map_widgets.dart
+++ oceanpredict_app/lib/widgets/map/map_widgets.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'map_service.dart';
+import '../../services/map_service.dart';
 
 // ============================================================
 // Search bar
